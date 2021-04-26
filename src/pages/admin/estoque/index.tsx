2b113cb0f94048e07fa3f65f0aed4ffb5c0df39b
Erick Martim <erick.martim@i9xp.com.br>
2021-04-26 15:32:48 -0300

    Listagem de Estoque

--- src/pages/admin/estoque/index.tsx
+++ src/pages/admin/estoque/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { Button, TextField } from '@material-ui/core';
@@ -6,7 +6,44 @@ import { faBox } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import withGuard from 'utils/withGuard';
 
+import Paper from '@material-ui/core/Paper';
+import { RowDetailState } from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  TableRowDetail,
+} from '@devexpress/dx-react-grid-material-ui';
+
+const RowDetail = ({ row }) => {
+  console.log(row);
+  return (
+  <div>
+    Details for
+    {' '}
+    {row.name}
+    {' '}
+    from
+    {' '}
+    {row.city}
+  </div>
+)};
+
 function Estoque() {  
+  const [columns] = useState([
+    { name: 'name', title: 'Produto' },
+    { name: 'quantity', title: 'Quantidade Disponível' },
+    { name: 'update_date', title: 'Últ. Atualização'}
+  ]);
+  const [rows] = useState([
+    { name: "Camisa Preta P", quantity: 20, update_date: null },
+    { name: "Camisa Preta M", quantity: 50, update_date: null },
+    { name: "Camisa Preta G", quantity: 30, update_date: null },
+    { name: "Camisa Preta GG", quantity: 50, update_date: null },
+    { name: "Camisa Preta XGG", quantity: 60, update_date: null }
+  ]);
+  const [expandedRowIds, setExpandedRowIds] = useState<(string|number)[]>([2, 5]);
+  
   return (
     <>
       <Box my={4} display="flex" justifyContent="space-between">
@@ -32,6 +69,21 @@ function Estoque() {
       />
 
       <Box my={4}>
+        <Paper>
+          <Grid
+            rows={rows}
+            columns={columns}
+          >
+            <RowDetailState
+              defaultExpandedRowIds={[2, 5]}
+            />
+            <Table />
+            <TableHeaderRow />
+            <TableRowDetail
+              contentComponent={RowDetail}
+            />
+          </Grid>
+        </Paper>
       </Box>
     </>
   );
